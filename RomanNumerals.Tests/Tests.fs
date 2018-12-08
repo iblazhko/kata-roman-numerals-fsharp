8a2974a0461cebee022ce2720bc316958eed34c1
Ivan Blazhko <ivan.blazhko@gmail.com>
2018-12-08 19:59:36 +0000

    (Red) 1 translates to I

--- RomanNumerals.Tests/Tests.fs
+++ RomanNumerals.Tests/Tests.fs
@@ -9,3 +9,7 @@ open FsUnit.Xunit
 [<Fact>]
 let ``0 translates to empty string`` () =
     romanize 0 |> should equal ""
+
+[<Fact>]
+let ``1 translates to I`` () =
+    romanize 1 |> should equal "I"
