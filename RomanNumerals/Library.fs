41d40dd8c07820bc267f697d2aad70e94c10fcc7
Ivan Blazhko <ivan.blazhko@gmail.com>
2018-12-09 08:46:27 +0000

    (Green) Complete test coverage for subtractive notation

--- RomanNumerals/Library.fs
+++ RomanNumerals/Library.fs
@@ -12,6 +12,11 @@ module Transformer =
             1000, "M"
 
             4, "IV"
+            9, "IX"
+            40, "XL"
+            90, "XC"
+            400, "CD"
+            900, "CM"
         ]
         |> List.sortDescending
 
