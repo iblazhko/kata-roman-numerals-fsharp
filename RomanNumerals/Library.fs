89b2c7b9f41b1b1ae9275ab22cd07ff2115db327
Ivan Blazhko <ivan.blazhko@gmail.com>
2018-12-09 08:00:14 +0000

    (Refactor) Additive pattern calculation

--- RomanNumerals/Library.fs
+++ RomanNumerals/Library.fs
@@ -1,9 +1,24 @@
 ﻿namespace RomanNumerals
 
 module Transformer =
+    // size -> numerals
+    let buckets =
+        [   1, "I";
+            5, "V";
+        ]
+        |> List.sortDescending
+
+    let findLargestFullBucker number =
+        buckets
+        |> Seq.filter (fun (size,_) -> size <= number)
+        |> Seq.head
+
+    let rec processBucket numerals reminder =
+        match reminder with
+        | 0 -> numerals |> String.concat ""
+        | n when n > 0 ->
+            let bucketSize, bucketNumerals = (findLargestFullBucker n)
+            processBucket (List.append numerals [bucketNumerals]) (reminder - bucketSize)
+
     let romanize number =
-        match number with
-        | 0 -> ""
-        | 1 -> "I"
-        | 2 -> "II"
-        | 5 -> "V"
+        processBucket [] number
