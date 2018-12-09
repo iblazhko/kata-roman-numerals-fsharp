e9c39a032bf78ed7a3209b1b48f0af3c1a2fbafa
Ivan Blazhko <ivan.blazhko@gmail.com>
2018-12-09 08:35:32 +0000

    (Red) Full set of single letter numerals

--- RomanNumerals.Tests/Tests.fs
+++ RomanNumerals.Tests/Tests.fs
@@ -11,17 +11,35 @@ let ``0 translates to empty string`` () =
     romanize 0 |> should equal ""
 
 [<Fact>]
-let ``1 translates to I`` () =
-    romanize 1 |> should equal "I"
-
-[<Fact>]
-let ``5 translates to V`` () =
-    romanize 5 |> should equal "V"
-
-[<Fact>]
 let ``2 translates to II`` () =
     romanize 2 |> should equal "II"
 
 [<Fact>]
 let ``4 translates to IV`` () =
     romanize 4 |> should equal "IV"
+
+type TestCase =
+    {
+        Value: int
+        ExpectedRoman: string
+    }
+
+type ``Roman numerals transformation`` () =
+    static member SingleLetterNumeralsTestCases
+        with get() =
+            [|
+                { Value =    1; ExpectedRoman = "I" }
+                { Value =    5; ExpectedRoman = "V" }
+                { Value =   10; ExpectedRoman = "X" }
+                { Value =   50; ExpectedRoman = "L" }
+                { Value =  100; ExpectedRoman = "C" }
+                { Value =  500; ExpectedRoman = "D" }
+                { Value = 1000; ExpectedRoman = "M" }
+            |]
+            |> Seq.map (fun (x) -> [| x |])
+            |> Seq.toArray
+
+    [<Theory>]
+    [<MemberData("SingleLetterNumeralsTestCases")>]
+    member verify.``Single letter roman numerals`` (testCase:TestCase) =
+        romanize(testCase.Value) |> should equal testCase.ExpectedRoman
