2c94d49275b3ce5ccc7540a68f2083c5748172c6
Ivan Blazhko <ivan.blazhko@gmail.com>
2018-12-09 08:37:05 +0000

    (Green) Full set of single letter numerals

--- RomanNumerals/Library.fs
+++ RomanNumerals/Library.fs
@@ -3,8 +3,13 @@
 module Transformer =
     // size -> numerals
     let buckets =
-        [   1, "I";
-            5, "V";
+        [   1, "I"
+            5, "V"
+            10, "X"
+            50, "L"
+            100, "C"
+            500, "D"
+            1000, "M"
 
             4, "IV"
         ]
